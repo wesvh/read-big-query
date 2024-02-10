2b9f997b4c170bf7adc72dca7dc5f0e19b716e10
Esteban V <evillada@qvision.com.co>
2024-02-10 09:05:19 -0500

    Actualizar ReadBigQueryModule y App.tsx

--- android/src/main/java/expo/modules/readbigquery/ReadBigQueryModule.kt
+++ android/src/main/java/expo/modules/readbigquery/ReadBigQueryModule.kt
@@ -10,19 +10,31 @@ import android.util.Log
 class ReadBigQueryModule : Module() {
     override fun definition() = ModuleDefinition {
         Name("ReadBigQuery")
-        AsyncFunction("readDictionary") { args: Array<Any>, promise: Promise ->
+        AsyncFunction("readDictionary") { dataSerialized: String,  promise: Promise ->
             try {
                 Log.d("ReadBigQueryModule", "Iniciando la conversión del diccionario")
-                val jsonString = args[0] as? String ?: throw IllegalArgumentException("Se esperaba un string JSON como argumento")
                 
                 val gson = Gson()
                 val type = object : TypeToken<List<List<Any>>>() {}.type
-                val data: List<List<Any>> = gson.fromJson(jsonString, type)
+                val data: List<List<Any>> = gson.fromJson(dataSerialized, type)
+                val dictionary = data[0] as List<String>
+                val values = data.subList(1, data.size)
 
-                // Aquí realizas las operaciones necesarias con `data`...
+                Log.d("ReadBigQueryModule", "Diccionario: $dictionary")
+
+                // ahora la idea es hacer un JSON con el diccionario y los valores, donde el diccionario sea el nombre de las llaves y los valores sean los valores de las llaves
+
+                val result = mutableListOf<Map<String, Any>>()
+                for (value in values) {
+                    val map = mutableMapOf<String, Any>()
+                    for (i in dictionary.indices) {
+                        map[dictionary[i]] = value[i]
+                    }
+                    result.add(map)
+                }
 
                 // Supongamos que simplemente convertimos `data` de nuevo a un string JSON para el ejemplo.
-                val resultString = gson.toJson(data)
+                val resultString = gson.toJson(result)
                 promise.resolve(resultString)
             } catch (e: Exception) {
                 promise.reject("ERROR_READ_DICTIONARY", "Failed to read dictionary", e)
--- example/App.tsx
+++ example/App.tsx
@@ -10,22 +10,36 @@ export default function App() {
         onPress={async () => {
           const performanced = performance.now();
           console.log("EXECUTING");
-          const JSONSTRING = JSON.stringify([
-            ["a", "b", "c"],
-            ["d", "e", "f"],
-            ["g", "h", "i"],
-          ]);
+          const JSONSTRING = JSON.stringify(ArrayMock);
           console.log(
             `El tiempo de ejecución fue de ${((performance.now() - performanced) / 1000).toFixed(2)}s.`
           );
-          await ReadBigQuery.readDictionary(JSONSTRING);
-          console.log(
-            `2 El tiempo de ejecución fue de ${((performance.now() - performanced) / 1000).toFixed(2)}s.`
-          );
-          console.log("Finalizo");
+          console.log("typeof JSONSTRING", typeof JSONSTRING);
+          ReadBigQuery.readDictionary(JSONSTRING)
+            .then((result) => {
+              console.log("result", typeof result);
+              const parsedJSON = JSON.parse(result);
+              console.log("parsedJSON", typeof parsedJSON);
+            })
+            .catch((error) => console.error(error))
+            .finally(() =>
+              console.log(
+                `2 El tiempo de ejecución fue de ${((performance.now() - performanced) / 1000).toFixed(2)}s.`
+              )
+            );
         }}
       >
-        <Text>Open up App.tssssx to start working on your app!</Text>
+        <View
+          style={{
+            //style like a pressableButton
+            backgroundColor: "blue",
+            padding: 10,
+            borderRadius: 10,
+            margin: 10,
+          }}
+        >
+          <Text>Open up App.tssssx to start working on your app!</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
